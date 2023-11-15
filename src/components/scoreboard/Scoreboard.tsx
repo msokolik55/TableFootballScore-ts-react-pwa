bc6d50d821fb780d424218e7d2d099ab35097407
Michal Sokolik <msokolik55@gmail.com>
2023-11-15 22:27:14 +0100

    feat: export stats as txt file

--- src/components/scoreboard/Scoreboard.tsx
+++ src/components/scoreboard/Scoreboard.tsx
@@ -3,6 +3,7 @@ import { teamHomeAtom, teamAwayAtom, periodsAtom } from "../../atom";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { Period } from "../../types";
 import { sumScore } from "./parsing";
+import { saveAs } from "file-saver";
 
 const Scoreboard = () => {
 	const [teamHome, setTeamHome] = useRecoilState(teamHomeAtom);
@@ -11,6 +12,31 @@ const Scoreboard = () => {
 
 	const parsePeriod = (period: Period) => {
 		return `${sumScore(period.home)}:${sumScore(period.away)}`;
+	};
+
+	const formatPlayer = (player: { name: string; goals: number }) => {
+		return `${player.name} - ${player.goals}`;
+	};
+
+	const exportMatch = () => {
+		const header = `${teamHome.name} : ${teamAway.name}\n\n`;
+		const text = periods
+			.map((period, idx) => {
+				const score = `${idx + 1}. zápas: ${sumScore(
+					period.home
+				)}:${sumScore(period.away)}`;
+
+				const home = period.home.players.map(formatPlayer);
+				const away = period.away.players.map(formatPlayer);
+				const players = [home.join("\n"), away.join("\n")].join("\n\n");
+
+				return `${score}\n\n${players}`;
+			})
+			.join(`\n\n${"-".repeat(25)}\n\n`);
+
+		const blob = new Blob([header, text], { type: "text/plain" });
+		const fileName = `${teamHome.abbreviation}-${teamAway.abbreviation}.txt`;
+		saveAs(blob, fileName);
 	};
 
 	return (
@@ -47,6 +73,9 @@ const Scoreboard = () => {
 			>
 				<h2>({periods.map(parsePeriod).join(", ")})</h2>
 			</div>
+			<button style={{ marginBottom: "1em" }} onClick={exportMatch}>
+				export
+			</button>
 		</>
 	);
 };
