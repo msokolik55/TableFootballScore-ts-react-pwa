22f70eccf33f253268b3d4209fe124a6b85c59ca
Michal Sokolik <msokolik55@gmail.com>
2024-04-09 12:24:17 +0200

    fix: format of teams.ts

--- src/teams.ts
+++ src/teams.ts
@@ -1,79 +1,79 @@
 import { Team } from "./types";
 
 export const TEAMS: Team[] = [
-  {
-    abbreviation: "PAC",
-    name: "PAPRICE",
-    players: [{ name: "Berta Papulová", name: "Barbora Tušilová" }]
-  },
-  {
-  abbreviation: "MID",
-    name: "MICHAL DAVID",
-    players: [{ name: "Dávid Fejczo", name: "Michal Fridrich" }]
-  },
-  {
-  abbreviation: "LZM",
-    name: "LAMY Z MICHALOVIEC",
-    players: [{ name: "Lucia Kodrikova", name: "Aneta Králová" }]
-  },
-  {
-  abbreviation: "KYM",
-    name: "KRÁMY YETIHO MAMY",
-    players: [{ name: "Adam Majzlík", name: "Zuzana Sepešiová" }]
-  },
-  {
-  abbreviation: "H",&P
-    name: "HOKEJKY A PÁLENKY",
-    players: [{ name: "Matej Červeň", name: "Gregor Biznár" }]
-  },
-  {
-  abbreviation: "VRH",
-    name: "VYHRNUTÉ RUKÁVY",
-    players: [{ name: "Richard Truben", name: "Monika Kapustová" }]
-  },
-  {
-  abbreviation: "TUČ",
-    name: "TUČŇÁCI",
-    players: [{ name: "Andrej Šimurka", name: "Kuba Plhal" }]
-  },
-  {
-  abbreviation: "ORG",
-    name: "ORGANIZÁTORSKÁ ZLOŽKA",
-    players: [{ name: "Šaňo Eliáš", name: "Karol Kováč" }]
-  },
-  {
-  abbreviation: "TAP",
-    name: "TIMON A PUMBA",
-    players: [{ name: "Jozef Tadian", name: "Timotej Martykan" }]
-  },
-  {
-  abbreviation: "ZMK",
-    name: "ZEC MI KAR",
-    players: [{ name: "Mališ Adam", name: "Branislav Vlha" }]
-  },
-  {
-  abbreviation: "PFC",
-    name: "PFC",
-    players: [{ name: "Hráč 1", name: "Hráč 2" }]
-  },
-  {
-  abbreviation: "BBH",
-    name: "MÄKČEŇ A VOKÁŇ",
-    players: [{ name: "Matúš Kapičák", name: "Michal Soki Sokolík" }]
-  },
-  {
-  abbreviation: "VON",
-    name: "ZA KAŽDÝ PREHRATÝ ZÁPAS RÚČKA VON",
-    players: [{ name: "Rastislav Krutak", name: "Samuel Antal" }]
-  },
-  {
-  abbreviation: "VID",
-    name: "IDEME SI PO VIDLIČKU",
-    players: [{ name: "Betka Fuglíková", name: "Maťa Lysičanová" }]
-  },
-  {
-  abbreviation: "LAS",
-    name: "LASKAAAA",
-    players: [{ name: "Tomáš Marek", name: "Diana Gulčíková" }]
-  }
+	{
+		abbreviation: "PAC",
+		name: "PAPRICE",
+		players: [{ name: "Berta Papulová" }, { name: "Barbora Tušilová" }],
+	},
+	{
+		abbreviation: "MID",
+		name: "MICHAL DAVID",
+		players: [{ name: "Dávid Fejczo" }, { name: "Michal Fridrich" }],
+	},
+	{
+		abbreviation: "LZM",
+		name: "LAMY Z MICHALOVIEC",
+		players: [{ name: "Lucia Kodrikova" }, { name: "Aneta Králová" }],
+	},
+	{
+		abbreviation: "KYM",
+		name: "KRÁMY YETIHO MAMY",
+		players: [{ name: "Adam Majzlík" }, { name: "Zuzana Sepešiová" }],
+	},
+	{
+		abbreviation: "H&P",
+		name: "HOKEJKY A PÁLENKY",
+		players: [{ name: "Matej Červeň" }, { name: "Gregor Biznár" }],
+	},
+	{
+		abbreviation: "VRH",
+		name: "VYHRNUTÉ RUKÁVY",
+		players: [{ name: "Richard Truben" }, { name: "Monika Kapustová" }],
+	},
+	{
+		abbreviation: "TUČ",
+		name: "TUČŇÁCI",
+		players: [{ name: "Andrej Šimurka" }, { name: "Kuba Plhal" }],
+	},
+	{
+		abbreviation: "ORG",
+		name: "ORGANIZÁTORSKÁ ZLOŽKA",
+		players: [{ name: "Šaňo Eliáš" }, { name: "Karol Kováč" }],
+	},
+	{
+		abbreviation: "TAP",
+		name: "TIMON A PUMBA",
+		players: [{ name: "Jozef Tadian" }, { name: "Timotej Martykan" }],
+	},
+	{
+		abbreviation: "ZMK",
+		name: "ZEC MI KAR",
+		players: [{ name: "Mališ Adam" }, { name: "Branislav Vlha" }],
+	},
+	{
+		abbreviation: "PFC",
+		name: "PFC",
+		players: [{ name: "Hráč 1" }, { name: "Hráč 2" }],
+	},
+	{
+		abbreviation: "BBH",
+		name: "MÄKČEŇ A VOKÁŇ",
+		players: [{ name: "Matúš Kapičák" }, { name: "Michal Soki Sokolík" }],
+	},
+	{
+		abbreviation: "VON",
+		name: "ZA KAŽDÝ PREHRATÝ ZÁPAS RÚČKA VON",
+		players: [{ name: "Rastislav Krutak" }, { name: "Samuel Antal" }],
+	},
+	{
+		abbreviation: "VID",
+		name: "IDEME SI PO VIDLIČKU",
+		players: [{ name: "Betka Fuglíková" }, { name: "Maťa Lysičanová" }],
+	},
+	{
+		abbreviation: "LAS",
+		name: "LASKAAAA",
+		players: [{ name: "Tomáš Marek" }, { name: "Diana Gulčíková" }],
+	},
 ];
